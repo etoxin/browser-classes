777f6a4b6a09c2d9d56d46ea67d760901ec06633
etoxin <adam@etoxin.net>
2018-07-23 21:53:15 +1000

    Cleaned up test

--- test/browser-classes.spec.js
+++ test/browser-classes.spec.js
@@ -2,24 +2,23 @@ var assert = require('assert');
 import BrowserClasses from '../browser-classes.js';
 
 describe('BrowserClasses.isString()', function () {
-    it('expects isString to return true when passed a string', function () {
+    it('expects isString to return true when passed a string', () => {
         assert.equal(BrowserClasses.isString('My String'), true);
     });
 
-    it('expects isString to return false when passed a number', function () {
+    it('expects isString to return false when passed a number', () => {
         assert.equal(BrowserClasses.isString(12345), false);
     });
 
-    it('expects isString to return false when passed a function', function () {
+    it('expects isString to return false when passed a function', () => {
         assert.equal(BrowserClasses.isString(function () {return true}), false);
     });
 
-    it('expects isString to return false when passed a object', function () {
+    it('expects isString to return false when passed a object', () => {
         assert.equal(BrowserClasses.isString({test: true}), false);
     });
 
-    it('expects isString to return false when passed a boolean', function () {
+    it('expects isString to return false when passed a boolean', () => {
         assert.equal(BrowserClasses.isString(true), false);
     });
-
 });
